33e49cb711937397201984af3fd432f085268b42
Shishamo <shishamodesu@outlook.jp>
2025-06-16 20:23:46 +0900

    jakkan test kaita

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,4 +1,10 @@
 #include "pch.h"
+#include "Vec2D.h"
+#include "Main.h"
 
-TEST(TestCaseName, TestName) {
+TEST(Vec2DTest, Vec2DMoveTest) {
+	Vec2D vec1 = Vec2D(0.0f, 0.0f);
+	Vec2D vec2 = Vec2D(5.0f, 0.0f);
+	Vec2D vec = vec1 + vec2;
+	EXPECT_EQ(5.0f, vec.GetX());
 }
