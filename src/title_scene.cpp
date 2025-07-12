e4cce568ed888915e43a7eb577c3514f12e7089f
Shishamo <shishamodesu@outlook.jp>
2025-07-13 01:05:26 +0900

    Cng: Title Scene String Pos Change.

--- src/title_scene.cpp
+++ src/title_scene.cpp
@@ -20,7 +20,7 @@ namespace zenithstg {
 		}
 		CreateParticle(Vec2D(rng() % 1920, 1080), GamingColor(180), kParticleRect, kBlendAdd, 255, EaseType::kEaseInQuad, 120, 0, 0, 0, 0, 0, 0.5f, 0.0f, EaseType::kEaseInQuad, 120, 0, Rad(105 - rng() % 30), Rad(105 - rng() % 30), EaseType::kEaseInQuad, 120, 7 + rng() % 5, 0, EaseType::kEaseInQuad, 120);
 		MoveParticles();
-		DrawFormatStringToHandle(kCenterX - GetDrawFormatStringWidthToHandle(SafeAccess(font_types, kFontUi1), "Press Enter Key.") / 2, kCenterY + 256, Color(kColorWhite).GetDxColor(), SafeAccess(font_types, kFontUi1), "Press Enter Key.");
+		DrawFormatStringToHandle(kCenterX - GetDrawFormatStringWidthToHandle(SafeAccess(font_types, kFontUi1), "Press Enter Key.") / 2, kCenterY, Color(kColorWhite).GetDxColor(), SafeAccess(font_types, kFontUi1), "Press Enter Key.");
 		
 		if (GetAsyncKeyState(VK_RETURN) & (1 << 15)) {
 			for (auto& x : particles) {
